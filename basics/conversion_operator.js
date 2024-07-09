a1150175d1d6c25a3caf931e032bb4b133ff77ab
Kartikx2005 <kartikxdev2005@gmail.com>
2024-07-09 15:50:34 +0530

    datatype operations

--- basics/conversion_operator.js
+++ basics/conversion_operator.js
@@ -25,3 +25,25 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+// *************Operations******************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello "
+let str2 = "Kartik"
+let str3 = str1 + str2
+
+console.log(str3);
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32 
+
+console.log(+true) // 1
+console.log(+"") // 0
+
