503bd6c223731d81a2706abbc5704facdc72b0ee
Kartikx2005 <kartikxdev2005@gmail.com>
2024-07-09 15:26:41 +0530

    completed the datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // this will not run beacuse we are using node js not browser
+
+console.log(3
+    +
+    3
+) // this is not the good practice, code readability should be high
+
+let age = 19
+let name = "Kartik"
+let isLoggedIn = false
+let state;
+
+// number -> 2 to power 53
+// bigint 
+// string -> ""
+// boolean -> true / false
+// null -> standalone value
+// undefined
+// symbol -> unique 
+
+// object 
+
+console.log(typeof name)
+
+console.log(typeof null) // object 
+console.log(typeof undefined) // undefined
